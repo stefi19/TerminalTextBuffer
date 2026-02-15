f0d5dbaed06ea81f7cb7dbf9047adeb914916947
stefi19 <stefaniamozacu1@gmail.com>
2026-02-15 12:10:37 +0200

    Add Color enum for terminal colors

---
+++ TerminalTextBuffer/src/main/java/org/example/Color.java
@@ -0,0 +1,25 @@
+package org.example;
+
+/**
+ * Represents the 16 standard terminal colors plus a default color.
+ * These are the ANSI color codes commonly used in terminal emulators.
+ */
+public enum Color {
+    DEFAULT,
+    BLACK,
+    RED,
+    GREEN,
+    YELLOW,
+    BLUE,
+    MAGENTA,
+    CYAN,
+    WHITE,
+    BRIGHT_BLACK,
+    BRIGHT_RED,
+    BRIGHT_GREEN,
+    BRIGHT_YELLOW,
+    BRIGHT_BLUE,
+    BRIGHT_MAGENTA,
+    BRIGHT_CYAN,
+    BRIGHT_WHITE
+}
